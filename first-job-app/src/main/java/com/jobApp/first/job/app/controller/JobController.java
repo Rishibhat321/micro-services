c2c9b3d36a1713f4aa68ade5f7da98116322cf30
Rishib#321 <rishitab2209@gmail.com>
2025-03-14 20:30:52 +0530

    create job controller, entity

--- first-job-app/src/main/java/com/jobApp/first/job/app/controller/JobController.java
+++ first-job-app/src/main/java/com/jobApp/first/job/app/controller/JobController.java
@@ -1,7 +1,12 @@
 package com.jobApp.first.job.app.controller;
 
+import com.jobApp.first.job.app.entity.Job;
+
+import java.util.List;
+
 public class JobController {
 
-
-
+    public List<Job> findAll() {
+        return jobs;
+    }
 }
---
+++ first-job-app/src/main/java/com/jobApp/first/job/app/entity/Job.java
@@ -0,0 +1,11 @@
+package com.jobApp.first.job.app.entity;
+
+public class Job {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+}
